502354b3f290ad6885e7ebeaee87bc0faa6ccd36
FrodobabboDev <74934987+FrodobabboDev@users.noreply.github.com>
2024-11-25 20:03:27 -0800

    Add files via upload

---
+++ chapterTwo.java
@@ -0,0 +1,16 @@
+public class chapterTwo {
+    public static void main(String[] args) {
+
+        String myName = "Francesco";
+        System.out.println(myName);
+
+        String myJob = "Programmer";
+        System.out.println(myJob);
+
+        String myNameCopy = myName;
+        System.out.println(myNameCopy);
+
+
+
+    }
+}
